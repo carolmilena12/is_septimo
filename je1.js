76d36ed9afa0857000fbf0d21faa81dd76508436
milenacarola <milenacarola19@gmail.com>
2025-03-30 08:43:54 -0400

    ejemplo scope

--- je1.js
+++ je1.js
@@ -1,3 +1,24 @@
-let nombre
-nombre ="Pedrito"
-console.log(nombre)
+let pais = "Bolivia"
+
+{
+    var depto1 = "La Paz";
+    let depto2 = "Cochabamba";
+    let depto3 = "Santa Cruz";
+    var depto1 = "Sucre"
+    console.log(depto1);
+    console.log(pais);
+}
+
+console.log(pais);
+let paisB = "Argentina"
+
+{
+    let depto1 = "Buenos Aires";
+    let depto2 = "Rosario";
+    let depto3 = "Cordova";
+
+    console.log(depto1);
+    console.log(pais);
+    console.log(paisB)
+}
+console.log(depto1)
